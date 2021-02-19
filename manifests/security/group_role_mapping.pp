82c361be2895f2590a39ce84947f8649c0a1c5af
Jon Law <jonathan.law@weareact.com>
2021-02-19 11:33:48 +0000

    Ensure parent node exists before adding mapping

--- manifests/security/group_role_mapping.pp
+++ manifests/security/group_role_mapping.pp
@@ -3,6 +3,10 @@ define wildfly::security::group_role_mapping($role, $group = $title, $realm = un
 
   $_group = upcase($group)
 
+  wildfly::resource { "/core-service=management/access=authorization/role-mapping=${role}":
+    content => {},
+  }
+  ->
   wildfly::resource { "/core-service=management/access=authorization/role-mapping=${role}/include=group-${_group}":
     content => {
       'name'  => $group,
