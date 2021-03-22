61313ca9254e56d1f2b8a9dbac121a31d82c1ec9
Jon Law <jonathan.law@weareact.com>
2021-03-22 16:47:16 +0000

    Correct resource path

--- manifests/elytron/sasl_authentication_factory.pp
+++ manifests/elytron/sasl_authentication_factory.pp
@@ -7,7 +7,7 @@ define wildfly::elytron::sasl_authentication_factory (
   Array[Hash] $mechanisms                   = [],
 ) {
 
-  wildfly::resource { "/subsystem=elytron/http-authentication-factory=${title}":
+  wildfly::resource { "/subsystem=elytron/sasl-authentication-factory=${title}":
     content => {
       'security-domain'         => $security_domain,
       'sasl-server-factory' => $sasl_server_factory,
