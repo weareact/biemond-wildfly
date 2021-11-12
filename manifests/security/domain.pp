0f132eff8a876bda1acc4b017441d8e8b1c50954
Jon Law <jonathan.law@fujitsu.com>
2021-11-12 17:26:14 +0000

    fix variable reference

--- manifests/security/domain.pp
+++ manifests/security/domain.pp
@@ -36,7 +36,7 @@ define wildfly::security::domain(
 
   $login_modules.each |$login_module, $login_module_opts | {
     wildfly::security::login_module { $login_module:
-      * => $login_module
+      * => $login_module_opts
     }
   }
 
